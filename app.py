93d28660558b5e2b47cf6a6b54d4e0f0c5cedcbb
Its-suLav-D <dah19001@byui.edu>
2023-07-20 23:16:17 -0600

    added-swt

--- app.py
+++ app.py
@@ -3,28 +3,27 @@ import requests
 import json
 
 # Define the URL of the Lambda function
-url = 'https://ovsw43fsp76f4gaf7zn7lmpztm0jkqrb.lambda-url.ap-southeast-2.on.aws/'
+essay_score_url = 'https://ovsw43fsp76f4gaf7zn7lmpztm0jkqrb.lambda-url.ap-southeast-2.on.aws/'
+summarize_text_url = 'https://ycbavt4o65otge4hyqojzxri6e0alynh.lambda-url.ap-southeast-2.on.aws/'
 
-# Set up Streamlit
+# Automated Essay Scoring
 st.title('Automated Essay Scoring')
 prompt = st.text_input('Prompt')
 essay = st.text_area('Essay', height=200)
 
-
-if st.button('Predict'):
+if st.button('Predict Essay Score'):
     # Define the data to send
     data = {
         "prompt": prompt,
         "essay": essay
     }
 
-    with st.spinner('Predicting...'):
+    with st.spinner('Predicting Essay Score...'):
         # Send a POST request
-        response = requests.post(url, data=json.dumps(data))
+        response = requests.post(essay_score_url, data=json.dumps(data))
 
     # Parse the JSON response
     response_data = response.json()
-    print(response_data)
 
     # Extract the predictions
     content_prediction = round(response_data['content'])
@@ -34,3 +33,28 @@ if st.button('Predict'):
     st.write('Content: ', content_prediction)
     st.write('Development | Structure | Coherence: ', written_context_prediction)
 
+
+# Summarize Written Text Prediction
+st.title('Summarize Written Text Prediction')
+passage = st.text_area('Passage', height=200)
+response = st.text_area('Response')
+
+if st.button('Predict Summarized Text'):
+    # Define the data to send
+    data = {
+        "passage": passage,
+        "user_response": response
+    }
+
+    with st.spinner('Predicting Summarized Text...'):
+        # Send a POST request
+        response = requests.post(summarize_text_url, data=json.dumps(data))
+
+    # Parse the JSON response
+    response_data = response.json()
+
+    # Extract the predictions
+    content_prediction = response_data['content']
+
+    # Display the predictions
+    st.write('Content: ', content_prediction)
